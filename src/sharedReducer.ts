65eaa03ec203878603fe18b2b2bf7a932abdf82f
Ilya Beda <ir4y.ix@gmail.com>
2020-05-11 21:02:21 +0700

    Simplify shared reducer

--- src/sharedReducer.ts
+++ src/sharedReducer.ts
@@ -1,28 +1,15 @@
-import React, { useReducer, Reducer, useCallback, Dispatch, ReducerAction } from "react";
+import { useReducer, Reducer, useCallback, Dispatch, ReducerAction } from "react";
 import useBus, { dispatch as emit } from "./use-bus";
 import { uuidv4 } from "./utils";
-
-type SetStateAction<S> = {
-    type: "____setState",
-    state: S
-}
-
-function isSetStateAction<S>(a: any): a is SetStateAction<S> {
-    return a.type === "____setState"
-}
 
 export function createSharedReducer<S, A>(reducer: Reducer<S, A>, initial: S) {
     let mutableState: S = initial;
     const uniqBus = uuidv4();
     return {
         useSharedReducer: (): [S, Dispatch<ReducerAction<Reducer<S, A>>>] => {
-            const innerReducer = useCallback((state: S, action: A | { type: "____setState", state: S }) => {
-                if (isSetStateAction(action)) {
-                    mutableState = action.state
-                    return action.state;
-                } else {
-                    return reducer(state, action)
-                }
+            const innerReducer = useCallback((state: S, action: A) => {
+                mutableState = reducer(state, action)
+                return mutableState
             }, [reducer])
             const [currentState, innerDispath] = useReducer(innerReducer, mutableState)
             const dispath = (action: A) => {
